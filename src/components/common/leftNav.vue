37a922d2f709e7d420841c84bfce5c96862f891d
passengerW <759744044@qq.com>
2019-05-26 16:23:36 +0800

    订餐

---
+++ src/components/common/leftNav.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="left-nav">
+    <ul>
+        <li><i class="icon iconfont icon-dianpu"></i><div>店铺</div></li>
+        <li><i class="icon iconfont icon-hanbao"></i><div>汉堡</div></li>
+        <li><i class="icon iconfont icon-huiyuanqia"></i><div>会员卡</div></li>
+        <li><i class="icon iconfont icon-gouwucheman"></i><div>购物车</div></li>
+        <li><i class="icon iconfont icon-gongnengjianyi"></i><div>功能建议</div></li>
+        <li><i class="icon iconfont icon-goumai"></i><div>购买</div></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'leftNav'
+    }
+
+</script>
+
+
+<style>
+        .left-nav{
+           color:#fff;
+           font-size:10px;
+           height:600px;
+           background-color: #1D8ce0;
+           float:left;
+           width:5%;
+        }
+        .iconfont{
+           font-size:24px;
+        }
+        .left-nav ul{
+            padding:0px;
+            margin: 0px;
+        }
+        .left-nav li{
+            list-style: none;
+            text-align: center;
+            border-bottom:1px solid #20a0ff;
+            padding:10px;
+        }
+    </style>
